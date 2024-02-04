bb8e849e76d7cfe84b212518a4295c62c6181892
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-02-04 23:21:46 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static strictfp void main(String[] args) {
+        int age = 19;// Crete only memory and store the values
+        Integer age2 = 199; // Act as pointer and store the value in memory and stores the address
+        // primitive data types are immutable in nature it always create a new memory
+        // and changes in that memory only not on the original memory
+        // In java char value can store the unique values a--> all language like hindi,
+        // spanish
+        // Non-Decimal : byte, short, int, long -2 ki power r to 2 ki power r -1
+        // Decimal : float, double, char, boolean
+        long w = (long) 90.20f;
+
+        // refernce data type that is mutable or secondary
+        // String/String Buffer , Wrapper types , Big types
+    }
+}
