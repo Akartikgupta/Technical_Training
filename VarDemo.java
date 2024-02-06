6f37721d90b086f81ae2d182cbe83308ecab5304
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-02-06 22:36:55 +0530

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,12 @@
+public class VarDemo {
+    public static void main(String_Demo[] args) {
+        // One way to declare the variable
+        // Explicit way
+        int a = 10;
+        // Another way to declare
+        // Implicit way --> use a rule known as type Inference
+        var b = 100;
+        // for constant value use for hard code values
+        final int MAX = 100;
+    }
+}
