b5275c4654aa868189c8faa01ac33d134016632d
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-02-14 22:23:40 +0530

    Add files via upload

---
+++ Even_And_Odd_Place_Sum.java
@@ -0,0 +1,21 @@
+
+public class Even_And_Odd_Place_Sum {
+    public static void main(String[] args) {
+        int num = 8765;
+        int Oddsum = 0;
+        int Evensum = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            num = num / 10;
+            if (rem % 2 == 0) {
+                Evensum += rem;
+            } else {
+                Oddsum += rem;
+            }
+
+        }
+        System.out.println("Sum of Even Digits : " + Evensum);
+        System.out.println("Sum of Odd Digits : " + Oddsum);
+    }
+
+}
