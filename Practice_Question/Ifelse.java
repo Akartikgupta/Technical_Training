384ce8f5e338aa412ca314ed2bf7303f999445de
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-02-20 21:47:19 +0530

    Update Ifelse.java

--- Practice_Question/Ifelse.java
+++ Practice_Question/Ifelse.java
@@ -1,1 +1,30 @@
 
+public class Ifelse {
+    public static void main(String[] args) {
+        // Simple if else
+        // if(true){
+
+        // }else{
+
+        // }
+        // // Multi if else
+        // if(true){
+
+        // }else if(){
+
+        // }else{
+
+        // }
+        // // Inner if else
+        // if(){
+        // if(){
+
+        // }else{
+
+        // }
+        // }else{
+
+        // }
+    }
+}
+
