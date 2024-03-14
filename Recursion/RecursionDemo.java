8a2ec65889e1b279523da8c130ea756fd9574a01
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-03-14 22:57:59 +0530

    Update RecursionDemo.java

--- Recursion/RecursionDemo.java
+++ Recursion/RecursionDemo.java
@@ -1,1 +1,17 @@
+public class RecursionDemo {
+    static void show(int n) {
+        // Base Condition
+        // Termination case
+        if (n == 0) {
+            return; // exit from the function call
+        }
+        System.out.println("kartik " + n);// processing logic
+        show(n - 1);// small problem +recursion (cycle)
+        System.out.println("Gupta " + n);
+    }
+
+    public static void main(String[] args) {
+        show(5);
+    }
+}
 
