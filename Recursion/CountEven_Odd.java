8f831c5cf5be00ae41512d937ffbb8d81a6a6005
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-01 01:09:33 +0530

    Update CountEven_Odd.java

--- Recursion/CountEven_Odd.java
+++ Recursion/CountEven_Odd.java
@@ -1,14 +1,20 @@
 public class CountEven_Odd {
-    // static int[] CountEven_Odd(int num) {
-    // // if (num == 0) {
-    // // return 0;
-    // // }
-    // // int va = va + CountEven_Odd(num - 1);
-    // // if (va % 2 == 0) {
+    // Using an Array method
+    static int[] CountEven_Odd(int num) {
+        if (num == 0) {
+            int result[] = new int[2];
+            return result;
+        }
+        int result[] = CountEven_Odd(num - 1);
+        if (num % 2 == 0) {
+            result[0] += 1;
+        } else {
+            result[1] += 1;
+        }
+        return result;
 
-    // // }
-    // }
-
+    }
+// Using the void method
     static void CountEven_Odd(int num, int evenCount, int oddCount) {
         if (num == 0) {
             System.out.println(evenCount);
