6b69ac04e774305aa7a6dddb905b3fa1401a1de1
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-01 22:58:01 +0530

    Update Sorted_Array.java

--- RecursionWithArray/Sorted_Array.java
+++ RecursionWithArray/Sorted_Array.java
@@ -1,5 +1,20 @@
 
 public class Sorted_Array {
+    public static void isSorted2(int arr[], int index) {
+        // Base case
+        if (index == arr.length - 1) {
+            System.out.println("Sorted");
+            return;
+        }
+        if (arr[index] > arr[index + 1]) {
+            System.out.println("Not Sorted");
+            return;
+        }
+        // small problem is to check whether the element is lesser than the next one or
+        // not
+        isSorted2(arr, index + 1);
+    }
+
     public static boolean isSorted(int arr[], int index) {
         // Base case
         if (index == arr.length - 1) {
@@ -13,9 +28,8 @@ public class Sorted_Array {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 10, 20, 30, 140, 50 };
-        System.out.println(isSorted(arr, 0) ? "Sorted" : "Not Sorted");
-        ;
+        int arr[] = { 10, 20, 30, 40, 50 };
+        // System.out.println(isSorted(arr, 0) ? "Sorted" : "Not Sorted");
+        isSorted2(arr, 0);
     }
 }
-
