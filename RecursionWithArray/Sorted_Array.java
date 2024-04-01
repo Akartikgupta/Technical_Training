1786af312bad7be8c2128b6b00c313a784b3edad
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-01 22:32:54 +0530

    Update Sorted_Array.java

--- RecursionWithArray/Sorted_Array.java
+++ RecursionWithArray/Sorted_Array.java
@@ -1,1 +1,21 @@
 
+public class Sorted_Array {
+    public static boolean isSorted(int arr[], int index) {
+        // Base case
+        if (index == arr.length - 1) {
+            return true;
+        }
+        if (arr[index] > arr[index + 1]) {
+            return false;
+        }
+        // small problem
+        return isSorted(arr, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 140, 50 };
+        System.out.println(isSorted(arr, 0) ? "Sorted" : "Not Sorted");
+        ;
+    }
+}
+
