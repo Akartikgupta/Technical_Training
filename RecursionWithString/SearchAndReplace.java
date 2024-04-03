28f1050ca9209ce365115b32cc7ad16f948e12df
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-03 22:44:12 +0530

    Update SearchAndReplace.java

--- RecursionWithString/SearchAndReplace.java
+++ RecursionWithString/SearchAndReplace.java
@@ -1,1 +1,20 @@
+public class SearchAndReplace {
+    public static String SearchAndReplace(String str, char search, char replace) {
+        // Base Case
+        if (str.length() == 0) {
+            return "";
+        }
+        String smalString = SearchAndReplace(str.substring(1), search, replace);
+        if (str.charAt(0) == search) {
+            return replace + smalString;
+        } else {
+            return str.charAt(0) + smalString;
+        }
+    }
+
+    public static void main(String[] args) {
+        String re = SearchAndReplace("hello", 'l', 'x');
+        System.out.println(re);
+    }
+}
 
