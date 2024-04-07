0cd609f71d5a1302bf52ec07f5e453fc72457821
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-07 23:43:46 +0530

    Update BoardProblem.java

--- RecursionTree/BoardProblem.java
+++ RecursionTree/BoardProblem.java
@@ -1,1 +1,22 @@
+public class BoardProblem {
+    public static void boardProblem(int currentValue, int EndValue, String result) {
+        // base case
+        if (currentValue == EndValue) {
+            System.out.println(result + " , ");
+            return;
+        }
+        // for negative value
+        if (currentValue > EndValue) {
+            return;
+        }
+        // small problem
+        for (int dice = 1; dice <= 6; dice++) {
+            boardProblem(currentValue + dice, EndValue, result + dice);
+        }
+    }
+
+    public static void main(String[] args) {
+        boardProblem(0, 10, "");
+    }
+}
 
