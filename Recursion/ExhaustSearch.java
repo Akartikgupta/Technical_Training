9f0f30a2d9a2dd90099c9c1104f03d9a67cdd65e
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-04-10 23:38:58 +0530

    Add files via upload

---
+++ Recursion/ExhaustSearch.java
@@ -0,0 +1,14 @@
+public class ExhaustSearch {
+    public static void allPosAtoZ(int nLen, String result){
+        if(nLen==result.length()){
+            System.out.println(result+" ");
+            return ;
+        }
+        for(char alpha='A';alpha<='Z';alpha++){
+            if(alpha)
+        }
+    }
+    public static void main(String[] args) {
+        allPosAtoZ(2,"");
+    }
+}
