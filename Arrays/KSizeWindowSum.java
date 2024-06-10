fb6f459d1fb75564f98c78d7449733b7b46b482b
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2024-06-10 22:29:00 +0530

    Update KSizeWindowSum.java

--- Arrays/KSizeWindowSum.java
+++ Arrays/KSizeWindowSum.java
@@ -1,1 +1,20 @@
+public class KSizeWindowSum {
+    public static void main(String[] args) {
+        int max = Integer.MIN_VALUE;
+        int[] nums = { 3, -1, 4, 12, -8, 5, 6 };
+        int k = 5; // size of window
+        int sum = 0;
+        // compute the sum of first window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        max = sum;
+        // now compute the sum of rest of the window
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k]; // add the right part and minus the left part
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
 
